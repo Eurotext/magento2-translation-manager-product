e8ae0497b19a639638bbcbfa197e9202611ae90b
Matthias Walter <matthias.walter@mwltr.de>
2018-09-17 15:23:23 +0200

    ProductReceiver implementation

--- src/Receiver/ProductReceiver.php
+++ src/Receiver/ProductReceiver.php
@@ -8,14 +8,146 @@ declare(strict_types=1);
 
 namespace Eurotext\TranslationManagerProduct\Receiver;
 
+use Eurotext\RestApiClient\Api\Project\ItemV1ApiInterface;
+use Eurotext\RestApiClient\Request\Project\ItemGetRequest;
 use Eurotext\TranslationManager\Api\Data\ProjectInterface;
 use Eurotext\TranslationManager\Api\EntityReceiverInterface;
+use Eurotext\TranslationManagerProduct\Api\Data\ProjectProductInterface;
+use Eurotext\TranslationManagerProduct\Api\ProjectProductRepositoryInterface;
+use Eurotext\TranslationManagerProduct\ScopeConfig\ProductScopeConfigReader;
+use Eurotext\TranslationManagerProduct\Setup\EntitySchema\ProjectProductSchema;
+use GuzzleHttp\Exception\GuzzleException;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Framework\Api\SearchCriteriaBuilder;
+use Magento\Store\Model\StoreManagerInterface;
+use Psr\Log\LoggerInterface;
 
 class ProductReceiver implements EntityReceiverInterface
 {
+    /**
+     * @var ProjectProductRepositoryInterface
+     */
+    private $projectProductRepository;
+
+    /**
+     * @var SearchCriteriaBuilder
+     */
+    private $searchCriteriaBuilder;
+
+    /**
+     * @var ItemV1ApiInterface
+     */
+    private $itemApi;
+
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
+    /**
+     * @var StoreManagerInterface
+     */
+    private $storeManager;
+
+    /**
+     * @var ProductScopeConfigReader
+     */
+    private $productScopeConfig;
+
+    public function __construct(
+        ItemV1ApiInterface $itemApi,
+        ProjectProductRepositoryInterface $projectProductRepository,
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        ProductRepositoryInterface $productRepository,
+        ProductScopeConfigReader $productScopeConfig,
+        LoggerInterface $logger
+    ) {
+        $this->projectProductRepository = $projectProductRepository;
+        $this->searchCriteriaBuilder    = $searchCriteriaBuilder;
+        $this->itemApi                  = $itemApi;
+        $this->productRepository        = $productRepository;
+        $this->productScopeConfig       = $productScopeConfig;
+        $this->logger                   = $logger;
+    }
 
     public function receive(ProjectInterface $project): bool
     {
-        return true;
+        $result = true;
+
+        $projectId    = $project->getId();
+        $projectExtId = $project->getExtId();
+        $storeId      = $project->getStoreviewDst();
+
+        $this->logger->info(sprintf('receive project products project-id:%d', $projectId));
+
+        $this->searchCriteriaBuilder->addFilter(ProjectProductSchema::PROJECT_ID, $projectId);
+        $this->searchCriteriaBuilder->addFilter(ProjectProductSchema::EXT_ID, 0, 'gt');
+        $searchCriteria = $this->searchCriteriaBuilder->create();
+
+        $searchResult = $this->projectProductRepository->getList($searchCriteria);
+
+        $projectProducts = $searchResult->getItems();
+
+        foreach ($projectProducts as $projectProduct) {
+            /** @var $projectProduct ProjectProductInterface */
+            $itemExtId = $projectProduct->getExtId();
+            $productId = $projectProduct->getProductId();
+
+            try {
+                $product = $this->productRepository->getById($productId, true, $storeId);
+
+                $itemRequest = new ItemGetRequest($projectExtId, $itemExtId);
+
+                $itemGetResponse = $this->itemApi->get($itemRequest);
+
+                $item = $itemGetResponse->getItemData();
+
+                // @todo refactor mapping to a processor class
+                $product->setName($item->getDataValue('name'));
+
+                $attributesEnabled = $this->productScopeConfig->getAttributesEnabled();
+                foreach ($attributesEnabled as $attributeCode) {
+                    $customAttribute = $product->getCustomAttribute($attributeCode);
+
+                    if ($customAttribute === null) {
+                        // missing custom attribute, maybe due to not being set at the global product,
+                        // or the attribute has been removed but still is configured in the system.xml
+                        continue;
+                    }
+
+                    $newValue = $item->getDataValue($attributeCode);
+
+                    if (empty($newValue)) {
+                        // If there is no translated value do not set it
+                        continue;
+                    }
+
+                    $customAttribute->setValue($newValue);
+                }
+
+                $this->productRepository->save($product);
+
+                // @todo set status to imported
+                $this->projectProductRepository->save($projectProduct);
+
+                $this->logger->info(sprintf('product id:%d, ext-id:%d => success', $productId, $itemExtId));
+            } catch (GuzzleException $e) {
+                $message = $e->getMessage();
+                $this->logger->error(sprintf('product id:%d => %s', $productId, $message));
+                $result = false;
+            } catch (\Exception $e) {
+                $message = $e->getMessage();
+                $this->logger->error(sprintf('product id:%d => %s', $productId, $message));
+                $result = false;
+            }
+
+        }
+
+        return $result;
     }
 }
---
+++ src/Test/Integration/Receiver/ProductReceiverIntegrationTest.php
@@ -0,0 +1,122 @@
+<?php
+declare(strict_types=1);
+/**
+ * @copyright see LICENSE.txt
+ *
+ * @see LICENSE.txt
+ */
+
+namespace Eurotext\TranslationManagerProduct\Test\Integration\Seeder;
+
+use Eurotext\RestApiClient\Api\Project\ItemV1Api;
+use Eurotext\RestApiClient\Api\ProjectV1Api;
+use Eurotext\TranslationManager\Service\Project\CreateProjectService;
+use Eurotext\TranslationManager\Test\Builder\ConfigurationMockBuilder;
+use Eurotext\TranslationManager\Test\Integration\IntegrationTestAbstract;
+use Eurotext\TranslationManager\Test\Integration\Provider\ProjectProvider;
+use Eurotext\TranslationManagerProduct\Receiver\ProductReceiver;
+use Eurotext\TranslationManagerProduct\Repository\ProjectProductRepository;
+use Eurotext\TranslationManagerProduct\Sender\ProductSender;
+use Eurotext\TranslationManagerProduct\Test\Integration\Provider\ProjectProductProvider;
+
+class ProductReceiverIntegrationTest extends IntegrationTestAbstract
+{
+    /** @var ProjectProductRepository */
+    private $projectProductRepository;
+
+    /** @var ProductReceiver */
+    private $sut;
+
+    /** @var ProjectProductProvider */
+    private $projectProductProvider;
+
+    /** @var ProjectProvider */
+    private $projectProvider;
+
+    /** @var ProductSender */
+    private $productSender;
+
+    /** @var CreateProjectService */
+    private $createProject;
+
+    /** @var ProjectV1Api */
+    private $projectApi;
+
+    protected function setUp()
+    {
+        parent::setUp();
+
+        $configBuiler = new ConfigurationMockBuilder($this);
+        $config       = $configBuiler->buildConfiguration();
+
+        $itemApi = new ItemV1Api($config);
+
+        $this->sut = $this->objectManager->create(
+            ProductReceiver::class,
+            [
+                'itemApi' => $itemApi,
+            ]
+        );
+
+        $this->projectApi = new ProjectV1Api($config);
+
+        $this->createProject = $this->objectManager->create(
+            CreateProjectService::class,
+            ['projectApi' => $this->projectApi]
+        );
+
+        $this->productSender = $this->objectManager->create(
+            ProductSender::class,
+            [
+                'itemApi' => $itemApi,
+            ]
+        );
+
+        $this->projectProvider        = $this->objectManager->get(ProjectProvider::class);
+        $this->projectProductProvider = $this->objectManager->get(ProjectProductProvider::class);
+
+        $this->projectProductRepository = $this->objectManager->get(ProjectProductRepository::class);
+    }
+
+    /**
+     * @magentoDataFixture loadFixture
+     *
+     * @throws \Magento\Framework\Exception\CouldNotSaveException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function testItShouldReceiveProjectProducts()
+    {
+        $productId = 10;
+        $name      = __CLASS__ . '-product-receiver';
+
+        $project = $this->projectProvider->createProject($name);
+
+        $projectProduct1  = $this->projectProductProvider->createProjectProduct($project->getId(), $productId);
+        $projectProductId = $projectProduct1->getId();
+
+        // Create project at Eurotext
+        $resultProjectCreate = $this->createProject->execute($project);
+        $this->assertTrue($resultProjectCreate);
+
+        // Send Project Products to Eurotext
+        $resultSend = $this->productSender->send($project);
+        $this->assertTrue($resultSend);
+
+        // @todo trigger /api/v1/project/translate/{project}.json
+        $this->projectApi->translate($project->getExtId());
+
+        // Receive Project from Eurotext
+        $result = $this->sut->receive($project);
+
+        $this->assertTrue($result);
+
+        $projectProduct = $this->projectProductRepository->getById($projectProductId);
+        $this->assertGreaterThan(0, $projectProduct->getExtId());
+    }
+
+    public static function loadFixture()
+    {
+        include __DIR__ . '/../_fixtures/provide_products.php';
+    }
+}
