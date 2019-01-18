27ab836c10428a164f8f28d35d531e39a1312791
Matthias Walter <matthias.walter@mwltr.de>
2019-01-18 16:15:43 +0100

    ETM-35 ProductSeeder logs Products not found to log / console

--- src/Seeder/ProductSeeder.php
+++ src/Seeder/ProductSeeder.php
@@ -17,6 +17,7 @@ use Eurotext\TranslationManagerProduct\Setup\ProjectProductSchema;
 use Magento\Catalog\Api\Data\ProductInterface;
 use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Api\SearchCriteriaBuilder;
+use Psr\Log\LoggerInterface;
 
 /**
  * ProductSeeder
@@ -43,16 +44,23 @@ class ProductSeeder implements EntitySeederInterface
      */
     private $searchCriteriaBuilder;
 
+    /**
+     * @var LoggerInterface
+     */
+    private $logger;
+
     public function __construct(
         ProductRepositoryInterface $productRepository,
         ProjectProductFactory $projectProductFactory,
         ProjectProductRepositoryInterface $projectProductRepository,
-        SearchCriteriaBuilder $searchCriteriaBuilder
+        SearchCriteriaBuilder $searchCriteriaBuilder,
+        LoggerInterface $logger
     ) {
         $this->productRepository        = $productRepository;
         $this->projectProductFactory    = $projectProductFactory;
         $this->projectProductRepository = $projectProductRepository;
         $this->searchCriteriaBuilder    = $searchCriteriaBuilder;
+        $this->logger                   = $logger;
     }
 
     public function seed(ProjectInterface $project, array $entities = []): bool
@@ -68,8 +76,13 @@ class ProductSeeder implements EntitySeederInterface
         $searchResult = $this->productRepository->getList($searchCriteria);
 
         if ($searchResult->getTotalCount() === 0) {
+            // no products found, matching the criteria
+            $this->logger->warning('no products found, matching the criteria');
+
             return $result;
         }
+
+        $entitiesNotFound = array_flip($entities);
 
         // create project product configurations
         $products = $searchResult->getItems();
@@ -77,7 +90,11 @@ class ProductSeeder implements EntitySeederInterface
         $projectId = $project->getId();
         foreach ($products as $product) {
             /** @var $product ProductInterface */
-            $productId = (int)$product->getId();
+            $productId  = (int)$product->getId();
+            $productSku = $product->getSku();
+
+            // Found entity, so remove it from not found list
+            unset($entitiesNotFound[$productSku]);
 
             $this->searchCriteriaBuilder
                 ->addFilter(ProjectProductSchema::ENTITY_ID, $productId)
@@ -87,6 +104,8 @@ class ProductSeeder implements EntitySeederInterface
             $searchResults = $this->projectProductRepository->getList($searchCriteria);
 
             if ($searchResults->getTotalCount() >= 1) {
+                // product has already been added to project
+                $this->logger->info(sprintf('skipping product-id:%d already added', $productId));
                 continue;
             }
 
@@ -101,6 +120,14 @@ class ProductSeeder implements EntitySeederInterface
             } catch (\Exception $e) {
                 $result = false;
             }
+        }
+
+        // Log entites that where not found
+        if (count($entitiesNotFound) > 0) {
+            foreach ($entitiesNotFound as $sku => $value) {
+                $this->logger->error(sprintf('product-sku "%s" not found', $sku));
+            }
+
         }
 
         return $result;
--- src/Test/Unit/Seeder/ProductSeederUnitTest.php
+++ src/Test/Unit/Seeder/ProductSeederUnitTest.php
@@ -21,6 +21,7 @@ use Magento\Catalog\Api\ProductRepositoryInterface;
 use Magento\Framework\Api\SearchCriteria;
 use Magento\Framework\Api\SearchCriteriaBuilder;
 use Magento\Framework\Api\SearchResultsInterface;
+use Psr\Log\LoggerInterface;
 
 class ProductSeederUnitTest extends UnitTestAbstract
 {
@@ -35,6 +36,9 @@ class ProductSeederUnitTest extends UnitTestAbstract
 
     /** @var ProductRepositoryInterface|\PHPUnit_Framework_MockObject_MockObject */
     private $productRepository;
+
+    /** @var LoggerInterface|\PHPUnit_Framework_MockObject_MockObject */
+    private $logger;
 
     /** @var ProjectMockBuilder */
     private $projectBuilder;
@@ -52,6 +56,7 @@ class ProductSeederUnitTest extends UnitTestAbstract
         $this->projectProductFactory    = $this->createMock(ProjectProductFactory::class);
         $this->projectProductRepository = $this->createMock(ProjectProductRepositoryInterface::class);
         $this->searchCriteriaBuilder    = $this->createMock(SearchCriteriaBuilder::class);
+        $this->logger                   = $this->createMock(LoggerInterface::class);
 
         $this->sut = $this->objectManager->getObject(
             ProductSeeder::class, [
@@ -59,6 +64,7 @@ class ProductSeederUnitTest extends UnitTestAbstract
                 'projectProductFactory'    => $this->projectProductFactory,
                 'projectProductRepository' => $this->projectProductRepository,
                 'searchCriteriaBuilder'    => $this->searchCriteriaBuilder,
+                'logger'                   => $this->logger,
             ]
         );
     }
@@ -248,6 +254,63 @@ class ProductSeederUnitTest extends UnitTestAbstract
 
         $project = $this->projectBuilder->buildProjectMock();
 
+        $result = $this->sut->seed($project, $entities);
+
+        $this->assertTrue($result);
+    }
+
+    public function testItShouldLogEntitiesNotFound()
+    {
+        $projectId    = 33;
+        $totalCount   = 1;
+        $entityId     = 11;
+        $pEntityCount = 0;
+        $entity       = 'some-entity';
+        $entities     = [$entity, 'not-found-entity'];
+
+        $this->searchCriteriaBuilder->method('addFilter')
+                                    ->withConsecutive(
+                                        ['sku', $entities, 'in'],
+                                        [ProjectProductSchema::ENTITY_ID, $entityId],
+                                        [ProjectProductSchema::PROJECT_ID, $projectId]
+                                    )->willReturnSelf();
+        $this->searchCriteriaBuilder->expects($this->exactly(2))
+                                    ->method('create')
+                                    ->willReturnOnConsecutiveCalls(new SearchCriteria(), new SearchCriteria());
+
+        // Product
+        $product = $this->createMock(ProductInterface::class);
+        $product->expects($this->once())->method('getId')->willReturn($entityId);
+        $product->expects($this->once())->method('getSku')->willReturn($entity);
+
+        $productResult = $this->createMock(ProductSearchResultsInterface::class);
+        $productResult->expects($this->once())->method('getTotalCount')->willReturn($totalCount);
+        $productResult->expects($this->once())->method('getItems')->willReturn([$product]);
+
+        $this->productRepository->expects($this->once())->method('getList')->willReturn($productResult);
+
+        // Search existing project entity
+        $projectEntityResult = $this->createMock(SearchResultsInterface::class);
+        $projectEntityResult->expects($this->once())->method('getTotalCount')->willReturn($pEntityCount);
+
+        $this->projectProductRepository->expects($this->once())->method('getList')->willReturn($projectEntityResult);
+        $this->projectProductRepository->expects($this->once())->method('save');
+
+        // New project entity
+        $pProduct = $this->createMock(ProjectProductInterface::class);
+        $pProduct->expects($this->once())->method('setProjectId')->with($projectId);
+        $pProduct->expects($this->once())->method('setEntityId')->with($entityId);
+        $pProduct->expects($this->once())->method('setStatus')->with(ProjectProductInterface::STATUS_NEW);
+
+        $this->projectProductFactory->expects($this->once())->method('create')->willReturn($pProduct);
+
+        // project mock
+        $project = $this->projectBuilder->buildProjectMock();
+        $project->expects($this->once())->method('getId')->willReturn($projectId);
+
+        $this->logger->expects($this->once())->method('error');
+
+        // TEST
         $result = $this->sut->seed($project, $entities);
 
         $this->assertTrue($result);
